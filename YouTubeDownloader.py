d6436d39961ca1914eaeb4e38553321772b7d496
Vaibhav Singh <88562515+NewEthicker@users.noreply.github.com>
2023-03-14 09:43:19 -0700

    Update YouTubeDownloader.py

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -9,9 +9,9 @@ while True:
         yt = YouTube(url)
         streams = yt.streams.filter(progressive=True, file_extension='mp4')
         for i, stream in enumerate(streams):
-            print(f"{i}. {stream}")
+            print(f"{i+1}. {stream}")
         choice = int(input("Enter the stream number to download: "))
-        streams[choice].download()
+        streams[choice-1].download()
         print("Video downloaded successfully.")
     elif choice == '2':
         exit()
