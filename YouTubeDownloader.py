e939dce3745a3e66919f5d65a6e32c516b7d4203
Vaibhav Singh <88562515+NewEthicker@users.noreply.github.com>
2023-03-14 08:06:10 -0700

    Add files via upload

---
+++ YouTubeDownloader.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+
+while True:
+    print("1. Download video by URL")
+    print("2. Exit")
+    choice = input("Enter your choice: ")
+    if choice == '1':
+        url = input("Enter the YouTube video URL: ")
+        yt = YouTube(url)
+        streams = yt.streams.filter(progressive=True, file_extension='mp4')
+        for i, stream in enumerate(streams):
+            print(f"{i}. {stream}")
+        choice = int(input("Enter the stream number to download: "))
+        streams[choice].download()
+        print("Video downloaded successfully.")
+    elif choice == '2':
+        exit()
+    else:
+        print("Invalid choice. Please try again.")
